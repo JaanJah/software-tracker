4aec2c9d415a5a5f77edd50eb5ad22747f4ef56c
JaanJahimees <jaanjahilo@outlook.com>
2019-12-27 21:33:49 +0200

    Use worker class instead putting everything in Main

--- SoftwareTracker/SoftwareTracker/Program.cs
+++ SoftwareTracker/SoftwareTracker/Program.cs
@@ -1,31 +1,13 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-
-namespace SoftwareTracker
+﻿namespace SoftwareTracker
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> links = LoadJson();
+            Worker worker = new Worker();
 
-            List<string> owners = new List<string>();
-            List<string> repos = new List<string>();
-            foreach (var item in links)
-            {
-                owners.Add(item.Split("/")[0]);
-                repos.Add(item.Split("/")[1]);
-            }
+            var ownersAndRepos = worker.GetOwnersAndRepos();
             
-        }
-
-
-        static List<string> LoadJson()
-        {
-            string json = File.ReadAllText($"{Directory.GetCurrentDirectory()}/links.json");
-            return JsonConvert.DeserializeObject<Links>(json).links;
         }
     }
 }
---
+++ SoftwareTracker/SoftwareTracker/Worker.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace SoftwareTracker
+{
+    class Worker
+    {
+        private readonly HttpClient httpClient;
+
+        public Worker()
+        {
+            httpClient = new HttpClient();
+        }
+
+
+        /// <summary>
+        /// Load links.json file
+        /// </summary>
+        private List<string> LoadJson()
+        {
+            string json = File.ReadAllText($"{Directory.GetCurrentDirectory()}/links.json");
+            return JsonConvert.DeserializeObject<Links>(json).links;
+        }
+
+        /// <summary>
+        /// Parse owner and repos from JSON
+        /// </summary>
+        public Tuple<List<string>, List<string>> GetOwnersAndRepos()
+        {
+            // Get links from JSON
+            List<string> links = LoadJson();
+
+            // Store owners/repos in lists
+            List<string> owners = new List<string>();
+            List<string> repos = new List<string>();
+
+            // Parse owners/repos from links
+            foreach (var item in links)
+            {
+                owners.Add(item.Split("/")[0]);
+                repos.Add(item.Split("/")[1]);
+            }
+            return new Tuple<List<string>, List<string>>(owners, repos);
+        }
+
+        public string GetLatestRelease()
+        {
+            // TODO
+            return null;
+        }
+    }
+}
